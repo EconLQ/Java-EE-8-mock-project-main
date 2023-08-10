659432ed710fa0020763b82777fb0aba4f829f52
econlq <zavaliybogdan@gmail.com>
2023-08-10 18:52:50 +0300

    ✅ Fixed ServletException with converting LocalDate to a String on the list.xhtml and confirm.xhtml

--- src/main/java/com/linkedin/hsportscatalogjsf/CatalogItemFormBean.java
+++ src/main/java/com/linkedin/hsportscatalogjsf/CatalogItemFormBean.java
@@ -1,45 +1,44 @@
 package com.linkedin.hsportscatalogjsf;
 
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.enterprise.context.SessionScoped;
-import javax.inject.Named;
 
 @SessionScoped
 @Named
 public class CatalogItemFormBean implements Serializable {
 
-	private CatalogItem item = new CatalogItem();
+    private CatalogItem item = new CatalogItem();
 
-	private List<CatalogItem> items = new ArrayList<>();
+    private List<CatalogItem> items = new ArrayList<>();
 
-	public String addItem() {
-		long itemId = this.items.size() + 1;
+    public String addItem() {
+        long itemId = this.items.size() + 1;
 
-		this.items.add(new CatalogItem(itemId, this.item.getName(), this.item.getManufacturer(),
-				this.item.getDescription(), this.item.getAvailableDate()));
-		
-		this.items.forEach(item -> System.out.println(item.toString()));
-		
-		return "list?faces-redirect=true";
-	}
+        this.items.add(new CatalogItem(itemId, this.item.getName(), this.item.getManufacturer(),
+                this.item.getDescription(), this.item.getAvailableDate()));
 
-	public CatalogItem getItem() {
-		return item;
-	}
+        this.items.forEach(item -> System.out.println(item.toString()));
 
-	public void setItem(CatalogItem item) {
-		this.item = item;
-	}
+        return "list?faces-redirect=true";
+    }
 
-	public List<CatalogItem> getItems() {
-		return items;
-	}
+    public CatalogItem getItem() {
+        return item;
+    }
 
-	public void setItems(List<CatalogItem> items) {
-		this.items = items;
-	}
+    public void setItem(CatalogItem item) {
+        this.item = item;
+    }
+
+    public List<CatalogItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<CatalogItem> items) {
+        this.items = items;
+    }
 
 }
