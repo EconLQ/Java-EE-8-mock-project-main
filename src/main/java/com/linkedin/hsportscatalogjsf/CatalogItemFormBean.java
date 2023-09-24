d0204686d79cf0b7a53a3c6df8ec08f8c2c62c43
econlq <zavaliybogdan@gmail.com>
2023-09-24 20:42:15 +0300

    💉 Injected LocalInventoryService and RemoteInventoryService beans to form

--- src/main/java/com/linkedin/hsportscatalogjsf/CatalogItemFormBean.java
+++ src/main/java/com/linkedin/hsportscatalogjsf/CatalogItemFormBean.java
@@ -1,6 +1,10 @@
 package com.linkedin.hsportscatalogjsf;
 
+import com.linkedin.hsportscatalogejb.CatalogItem;
+import com.linkedin.hsportscatalogejb.CatalogLocal;
+
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -9,20 +13,31 @@ import java.util.List;
 @SessionScoped
 @Named
 public class CatalogItemFormBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Inject
+    private CatalogLocal catalogBean;
+    @Inject
+    private InventoryService inventoryService;
+    @Inject
+    private InventoryService remoteInventoryService;
 
     private CatalogItem item = new CatalogItem();
 
     private List<CatalogItem> items = new ArrayList<>();
 
     public String addItem() {
-        long itemId = this.items.size() + 1;
+        long itemId = catalogBean.getItems().size() + 1;
 
-        this.items.add(new CatalogItem(itemId, this.item.getName(), this.item.getManufacturer(),
+        catalogBean.addItem(new CatalogItem(itemId, this.item.getName(), this.item.getManufacturer(),
                 this.item.getDescription(), this.item.getAvailableDate()));
 
-        this.items.forEach(item -> System.out.println(item.toString()));
-
+//        catalogBean.getItems().forEach(item -> System.out.println(item.toString()));
+        this.inventoryService.createItem(this.item.getItemId(), this.item.getName());
         return "list?faces-redirect=true";
+    }
+
+    public void init() {
+        this.items = this.catalogBean.getItems();
     }
 
     public CatalogItem getItem() {
