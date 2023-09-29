266b5be471326e19dc3a3d9b6cf3210137c320db
econlq <zavaliybogdan@gmail.com>
2023-09-29 23:43:20 +0300

    ➕ Added realization for the new methods documented in CatalogLocal interface.

--- src/main/java/com/linkedin/hsportscatalogejb/Catalog.java
+++ src/main/java/com/linkedin/hsportscatalogejb/Catalog.java
@@ -2,6 +2,8 @@ package com.linkedin.hsportscatalogejb;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Singleton;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +13,9 @@ import java.util.List;
 @Singleton
 @LocalBean
 public class Catalog implements CatalogLocal {
+    @PersistenceContext
+    private EntityManager entityManager;
+
     private List<CatalogItem> items = new ArrayList<>();
 
     public Catalog() {
@@ -18,11 +23,32 @@ public class Catalog implements CatalogLocal {
 
     @Override
     public List<CatalogItem> getItems() {
-        return items;
+        return this.entityManager.createQuery("select c from CatalogItem c", CatalogItem.class).getResultList();
     }
 
     @Override
     public void addItem(CatalogItem item) {
-        items.add(item);
+        this.entityManager.persist(item);
+    }
+
+    @Override
+    public CatalogItem findItem(Long itemId) {
+        return this.entityManager.find(CatalogItem.class, itemId);
+    }
+
+    @Override
+    public void deleteItem(CatalogItem catalogItem) {
+        this.entityManager.remove(
+                this.entityManager.contains(catalogItem)
+                        ? catalogItem
+                        : this.entityManager.merge(catalogItem));
+    }
+
+    @Override
+    public List<CatalogItem> searchByName(String name) {
+        return this.entityManager.createQuery("select c from CatalogItem c " +
+                        "where c.name=:name", CatalogItem.class)
+                .setParameter("name", "%" + name + "%")
+                .getResultList();
     }
 }
